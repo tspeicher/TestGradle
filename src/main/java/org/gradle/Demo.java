9df1adfb9f1852490a72e55f09daf63a86a51a9b
t.speicher <t.speicher@temeka.de>
2015-05-06 15:22:16 +0200

    Test-Projekt commited

---
+++ src/main/java/org/gradle/Demo.java
@@ -0,0 +1,11 @@
+package org.gradle;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		System.out.println("Gradle Test");
+		Person p = new Person("Test-Person");
+		System.out.println("Hello " + p.getName());
+	}
+
+}
