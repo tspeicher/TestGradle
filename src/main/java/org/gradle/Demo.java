55673ffc3a36341c1ca150589ba02766c3894aab
t.speicher <t.speicher@temeka.de>
2015-05-07 13:12:21 +0200

    Test commit 3f

--- src/main/java/org/gradle/Demo.java
+++ src/main/java/org/gradle/Demo.java
@@ -3,7 +3,7 @@ package org.gradle;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Gradle Test 3g");
+		System.out.println("Gradle Test 4");
 		Person p = new Person("Test-Person");
 		System.out.println("Hello " + p.getName());
 	}
