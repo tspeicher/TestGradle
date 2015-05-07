7c3116e8e0b3c6b5ed69d3271a1a6512ac2a06e9
t.speicher <t.speicher@temeka.de>
2015-05-07 10:23:57 +0200

    Test 2

--- src/main/java/org/gradle/Demo.java
+++ src/main/java/org/gradle/Demo.java
@@ -3,7 +3,7 @@ package org.gradle;
 public class Demo {
 
 	public static void main(String[] args) {
-		System.out.println("Gradle Test");
+		System.out.println("Gradle Test 2");
 		Person p = new Person("Test-Person");
 		System.out.println("Hello " + p.getName());
 	}
